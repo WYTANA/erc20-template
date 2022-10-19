16c315078351540d1d841c8d82040b28f125390d
torback <tlorback@gmail.com>
2022-10-19 16:51:06 -0600

    Assign total supply and track balances

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,6 +1,7 @@
+const hre = require("hardhat")
+
 async function main() {
-  // eslint-disable-next-line no-undef
-  const Token = await ethers.getContractFactory("Token")
+  const Token = await hre.ethers.getContractFactory("Token")
   const token = await Token.deploy()
 
   await token.deployed()
--- test/Token.js
+++ test/Token.js
@@ -6,11 +6,14 @@ const tokens = (n) => {
 }
 
 describe("Token", () => {
-  let token
+  let token, accounts, deployer
 
   beforeEach(async () => {
     const Token = await ethers.getContractFactory("Token")
     token = await Token.deploy("Black Hills Token", "BHT", "1000000")
+
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
   })
 
   describe("Deployment", () => {
@@ -33,6 +36,10 @@ describe("Token", () => {
 
     it("has correct total supply ...", async () => {
       expect(await token.totalSupply()).to.equal(totalSupply)
+    })
+
+    it("assigns total supply to deployer ...", async () => {
+      expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
     })
   })
 })
