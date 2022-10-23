f615f1e9048313849d812875975886016f1851ea
torback <tlorback@gmail.com>
2022-10-23 00:19:11 -0600

    Add approve function, allowance mapping, approval event, and testing for all

--- test/Token.js
+++ test/Token.js
@@ -6,7 +6,7 @@ const tokens = (n) => {
 }
 
 describe("Token", () => {
-  let token, accounts, deployer, receiver
+  let token, accounts, deployer, receiver, exchange
 
   beforeEach(async () => {
     const Token = await ethers.getContractFactory("Token")
@@ -15,6 +15,7 @@ describe("Token", () => {
     accounts = await ethers.getSigners()
     deployer = accounts[0]
     receiver = accounts[1]
+    exchange = accounts[2]
   })
 
   describe("Deployment", () => {
@@ -83,6 +84,42 @@ describe("Token", () => {
         const invalidAddress = "0x0000000000000000000000000000000000000000"
         await expect(token.connect(deployer).transfer(invalidAddress, amount))
           .to.be.reverted
+      })
+    })
+  })
+
+  describe("Approved tokens", () => {
+    let amount, transaction, result
+    beforeEach(async () => {
+      amount = tokens(100)
+      transaction = await token
+        .connect(deployer)
+        .approve(exchange.address, amount)
+      result = await transaction.wait()
+    })
+    describe("Successfully", () => {
+      it("allocate an allowance for delegated token spending", async () => {
+        expect(
+          await token.allowance(deployer.address, exchange.address)
+        ).to.equal(amount)
+      })
+
+      it("emit an approval event", async () => {
+        const event = result.events[0].event
+        expect(event).to.equal("Approval")
+
+        const args = result.events[0].args
+        expect(args.owner).to.equal(deployer.address)
+        expect(args.spender).to.equal(exchange.address)
+        expect(args.value).to.equal(amount)
+      })
+    })
+
+    describe("Fail", () => {
+      it("for invalid spender", async () => {
+        const invalidAddress = "0x0000000000000000000000000000000000000000"
+        await expect(token.connect(deployer).approve(invalidAddress, amount)).to
+          .be.reverted
       })
     })
   })
