2ba94637befa5b560d18c5c5fe3d66ac71a4224d
torback <tlorback@gmail.com>
2022-10-24 12:57:56 -0600

    Add env variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox")
-// require("dotenv").config()
+require("dotenv").config()
+const privateKeys = process.env.PRIVATE_KEY || ""
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   networks: {
     localhost: {},
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKeys.split(","),
+    },
   },
 }
--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -2,7 +2,7 @@ const hre = require("hardhat")
 
 async function main() {
   const Token = await hre.ethers.getContractFactory("Token")
-  const token = await Token.deploy()
+  const token = await Token.deploy("TLO Token", "TLO", "1000")
 
   await token.deployed()
 
